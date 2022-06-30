da4f5da4abb43e1fa8adbecde0f636609167ca5d
Yong Zheng <103731622+Lauyz@users.noreply.github.com>
2022-06-30 17:14:04 +0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,75 @@
+//Accept user input by just clicking on the "Enter" button
+const inputField = document.getElementById("input");
+inputField.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    let input = inputField.value;
+    inputField.value = "";
+    output(input);
+  }
+});
+
+// //Accepting the user inputs without the restriction of upper case, lower case letters and other words that can make the matches difficult
+// function output(input) {
+//   let product;
+//   let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+//   text = text
+//     .replace(/ a /g, " ")
+//     .replace(/whats/g, "what is")
+//     .replace(/please /g, "")
+//     .replace(/ please/g, "")
+//     .replace(/r u/g, "are you");
+
+//   if (compare(utterances, answers, text)) {
+//     // Search for exact match in triggers
+//     product = compare(utterances, answers, text);
+//   } 
+//   else {
+//     product = alternatives[Math.floor(Math.random() * alternatives.length)];
+//   }
+
+//   addChatEntry(input, product);
+// }
+ //Comparing the user input with the trigger phrases arrays
+// function compare(utterancesArray, answersArray, string) {
+//   let reply;
+//   let replyFound = false;
+//   for (let x = 0; x < utterancesArray.length; x++) {
+//     for (let y = 0; y < utterancesArray[x].length; y++) {
+//       if (utterancesArray[x][y] === string) {
+//         let replies = answersArray[x];
+//         reply = replies[Math.floor(Math.random() * replies.length)];
+//         replyFound = true;
+//         break;
+//       }
+//     }
+//     if (replyFound) {
+//       break;
+//     }
+//   }
+//   return reply;
+// }
+
+ //Update the DOM
+function addChatEntry(input, product) {
+  const messagesContainer = document.getElementById("messages");
+  let userDiv = document.createElement("div");
+  userDiv.id = "user";
+  userDiv.className = "user response";
+  userDiv.innerHTML = `<span>${input}</span>`;
+  messagesContainer.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  let botText = document.createElement("span");
+  botDiv.id = "bot";
+  botDiv.className = "bot response";
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  messagesContainer.appendChild(botDiv);
+
+  messagesContainer.scrollTop =
+    messagesContainer.scrollHeight - messagesContainer.clientHeight;
+ //setTimeout() is used for the bot to mimic the thinking of human, it will replies the message after two seconds
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+  }, 2000);
+}
